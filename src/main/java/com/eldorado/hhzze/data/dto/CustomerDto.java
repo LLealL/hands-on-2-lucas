5b8ea11b311594da9e219a4c5b2937a8d9c75dbc
Lucas <30868253+LLealL@users.noreply.github.com>
2023-01-21 20:51:31 -0300

    remocao de imports nao usados

--- src/main/java/com/eldorado/hhzze/data/dto/CustomerDto.java
+++ src/main/java/com/eldorado/hhzze/data/dto/CustomerDto.java
@@ -6,12 +6,11 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.sql.Date;
 import java.util.UUID;
